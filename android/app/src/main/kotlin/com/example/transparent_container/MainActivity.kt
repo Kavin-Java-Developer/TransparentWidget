648123b007837bd126628c81787cccd2d08d1bcf
kavin <kavink.s.it.2020@snsct.org>
2024-06-15 23:30:22 +0530

    transparent Container

---
+++ android/app/src/main/kotlin/com/example/transparent_container/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.transparent_container
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transparent Widget'),
+          backgroundColor: Colors.green,
+        ),
+        body: Container(
+          color: Colors.red,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Image.asset(
+                'assets/images/rohitsharma.jpg',
+                fit: BoxFit.contain,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(20)),
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.all(20),
+                child: Text('Rohit Sharma'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
